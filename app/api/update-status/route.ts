62e76da4c0b07d01fe267a123206cc682906d1f7
DHIRAJ Thapa <thapadhiraj468@gmail.com>
2026-06-03 15:54:07 +0530

    Remove test booking don

--- app/api/update-status/route.ts
+++ app/api/update-status/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://ryrsrrjfnmuitlccowvh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5cnNycmpmbm11aXRsY2Nvd3ZoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTgwMjY1OCwiZXhwIjoyMDk1Mzc4NjU4fQ.Pr_3-Toxr7bKuHb5wU6yZYBBg2LHc6aNv72u82fB4YQ"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 const resend = new Resend("re_FvLKTB8R_Q8FAGLMgrJL5MGoxZQQgLM59");
